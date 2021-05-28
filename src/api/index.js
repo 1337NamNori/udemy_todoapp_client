bc404b63a90eca448855564f9447b8e3bfd87049
1337NamNori <nam20051999@gmail.com>
2021-05-28 18:48:28 +0700

    Deploy prepare

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = `http://localhost:5000/todos`;
+const url = `https://udemy-mern-todoapp.herokuapp.com/todos`;
 
 export const readTodos = () => axios.get(url);
 export const createTodo = (newTodo) => axios.post(url, newTodo);
