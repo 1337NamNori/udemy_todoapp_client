0c069d2ff05cbb8f55a795084f67509378a8dfe4
1337NamNori <nam20051999@gmail.com>
2021-05-28 17:08:49 +0700

    Read and Create Function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { readTodos, createTodo } from './functions/index.js';
+import Loading from './components/loading/Loading.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todo, setTodo] = useState({ title: '', content: '' });
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await readTodos();
+            console.log(result);
+        };
+        fetchData();
+    }, []);
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const result = await createTodo(todo);
+        console.log(result);
+    };
+
+    return (
+        <div className="container">
+            <div className="row">
+                <pre>{JSON.stringify(todo)}</pre>
+                <form className="col s12" onSubmit={submitHandler}>
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <i class="material-icons prefix">title</i>
+                            <input
+                                id="title"
+                                type="text"
+                                className="validate"
+                                onChange={(e) =>
+                                    setTodo({ ...todo, title: e.target.value })
+                                }
+                            />
+                            <label htmlFor="title">Title</label>
+                        </div>
+                        <div className="input-field col s6">
+                            <i class="material-icons prefix">description</i>
+                            <input
+                                id="content"
+                                type="text"
+                                className="validate"
+                                onChange={(e) =>
+                                    setTodo({
+                                        ...todo,
+                                        content: e.target.value,
+                                    })
+                                }
+                            />
+                            <label htmlFor="content">Content</label>
+                        </div>
+                    </div>
+                    <div className="right-align">
+                        <button
+                            class="waves-effect waves-light btn"
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            <Loading />
+
+            <ul class="collection with-header">
+                <li class="collection-header">
+                    <h4>Todo List</h4>
+                </li>
+                <li class="collection-item">
+                    <div>
+                        Alvin
+                        <a href="#!" class="secondary-content">
+                            <i class="material-icons">delete</i>
+                        </a>
+                    </div>
+                </li>
+                <li class="collection-item">
+                    <div>
+                        Alvin
+                        <a href="#!" class="secondary-content">
+                            <i class="material-icons">delete</i>
+                        </a>
+                    </div>
+                </li>
+                <li class="collection-item">
+                    <div>
+                        Alvin
+                        <a href="#!" class="secondary-content">
+                            <i class="material-icons">delete</i>
+                        </a>
+                    </div>
+                </li>
+                <li class="collection-item">
+                    <div>
+                        Alvin
+                        <a href="#!" class="secondary-content">
+                            <i class="material-icons">delete</i>
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/functions/index.js
@@ -0,0 +1,19 @@
+import * as api from '../api/index.js';
+
+export const readTodos = async () => {
+    try {
+        const { data } = await api.readTodos();
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const createTodo = async (todo) => {
+    try {
+        const { data } = await api.createTodo(todo);
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+};
